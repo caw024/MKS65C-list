2781dfa92aa9d725f2375aa719d24a651bba2765
caw024 <caw@stuy.edu>
2018-10-10 15:11:51 -0400

    yeet

--- list.c
+++ list.c
@@ -13,20 +13,31 @@ void print_list(struct node * n){
   }
 }
 
-struct node * insert_front(struct node *, int);
+struct node * insert_front(struct node * n, int x){
+  struct node * a;
+  a->i = x;
+  a->next = &n;
+  return a;
+}
 
 struct node * free_list(struct node *);
 
 int main(){
   struct node s1,s2,s3,s4,s5;
+  struct node *L;
   s1.i = 1;
-  s1.next = s2;
+  s1.next = &s2;
   s2.i = 2;
-  s2.next = s3;
+  s2.next = &s3;
   s3.i = 3;
-  s3.next = s4;
+  s3.next = &s4;
   s4.i = 4;
-  s4.next = s5;
+  s4.next = &s5;
   s5.i = 5;
   s5.next = NULL;
+
+  print_list(s1.next);
+  L = insert_front(s1.next,10);
+  print_list(L);
+  
 }
